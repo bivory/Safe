0d77152f4169b57c6eac04d54cbd46d6618800a3
Bryan Ivory <bivory@gmail.com>
2013-05-27 14:09:42 -0700

    Refactored to support a state machine
    The scenario "Unsuccessfully setting the PIN to a safe" passes again

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,17 +1,78 @@
+class SafeState
+  attr_accessor :display
+  attr_accessor :input
+
+  def initialize(display_text="")
+    @input = {}
+    @input[:pin] = "PIN"
+
+    @display = display_text
+  end
+
+  def transition(input)
+    SafeState.new
+  end
+end
+
+class SafeStateError < SafeState
+  def initialize(display_text="ERROR")
+    super(display_text)
+  end
+end
+
+class SafeStateReady < SafeState
+  def transition(input)
+    case input
+    when @input[:pin]
+      SafeStatePin.new ""
+    else
+      SafeStateError.new
+    end
+  end
+end
+
+class SafeStatePin < SafeState
+  def initialize(display_text="ERROR")
+    super(display_text)
+    @value = ""
+  end
+
+  def transition(input)
+    case input
+    when @input[:pin]
+      if @value.length == 6
+        SafeStateReady.new ""
+      else
+        SafeStateError.new
+      end
+    when /^[0-9]$/
+      @value << input
+      @display = input
+      self
+    else
+      SafeStateError.new
+    end
+  end
+end
+
 class Safe
   attr_accessor :pin
-  attr_reader :display
   attr_accessor :lock
   attr_accessor :door
 
   def initialize
     @pin = "123456"
-    @display = "ERROR"
     @lock = :locked
     @door = :closed
+    @state = SafeStateReady.new
+  end
+
+  def display
+    @state.display
   end
 
   def press(button)
+    @state = @state.transition button
   end
 end
 
