353cf05e5984fdeafcc72decc6c4782043756a50
Bryan Ivory <bivory@gmail.com>
2013-05-27 12:55:56 -0700

    Start with the simplest case

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,12 @@
+Given(/^I have a safe$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the PIN should be (\d+)$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the PIN should be (\d+) digits$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
