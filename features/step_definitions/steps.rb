d6fd561f01437ec694f33a2fcd9e4a2c3b6b804c
Bryan Ivory <bivory@gmail.com>
2013-05-27 13:17:24 -0700

    Pass the first story

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,17 +1,20 @@
 class Safe
+  attr_accessor :pin
+
   def initialize
+    @pin = "123456"
   end
 end
 
 Given(/^I have a safe$/) do
-  Safe.new
+  @safe = Safe.new
 end
 
 Then(/^the PIN should be (\d+)$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  @safe.pin.should eq("123456")
 end
 
 Then(/^the PIN should be (\d+) digits$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  @safe.pin.length.should eq(6)
 end
 
