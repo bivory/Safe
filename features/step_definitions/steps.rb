e2347a5a067b0f0c7d980a82da59450b131c50b5
Bryan Ivory <bivory@gmail.com>
2013-05-27 13:21:23 -0700

    When I hit the PIN button

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -4,6 +4,9 @@ class Safe
   def initialize
     @pin = "123456"
   end
+
+  def press_pin
+  end
 end
 
 Given(/^I have a safe$/) do
@@ -18,3 +21,14 @@ Then(/^the PIN should be (\d+) digits$/) do |arg1|
   @safe.pin.length.should eq(6)
 end
 
+When(/^I hit the PIN button$/) do
+  @safe.press_pin
+end
+
+When(/^I hit the (\d+) button$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the display shows ERROR$/) do
+  pending # express the regexp above with the code you wish you had
+end
